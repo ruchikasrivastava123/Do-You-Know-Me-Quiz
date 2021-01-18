556df2bacbae39b7da871a22854bd632f27f8aa4
ruchika srivastava <ruchika1997.rs@gmail.com>
2021-01-18 17:07:00 +0530

    docs: added Do-You-Know-Me-Quiz
    Quiz based on Ruchika Srivastava

---
+++ index.js
@@ -0,0 +1,73 @@
+var readlinesync=require("readline-sync");
+var chalk=require("chalk");
+var username=readlinesync.question(chalk.bold.green
+("What's your name? " ));
+console.log("Hi!! "+chalk.italic(username));
+var score =0;
+var b=true;
+var know=readlinesync.question("Do you know Ruchika? ");
+if(know.toUpperCase()==='NO')
+b=false;
+function play( quest, ans)
+{
+var useranswer=readlinesync.question(quest);
+if(useranswer.toUpperCase()===ans.toUpperCase())
+{
+console.log("Yeah:) You're correct")
+score++;
+}
+else
+{
+console.log("ohhh"+":("+" You're wrong")
+score--;
+}
+
+}
+
+
+
+if(b)
+{
+var questionList=
+[
+  {
+  question: chalk.yellow("Where she lives? "),
+  answer:   "Lucknow"
+},
+{
+  question: chalk.yellowBright("Where she works? "),
+  answer:   "wipro"
+},
+{
+  question: chalk.blueBright("Her work location? "),
+  answer:   "mumbai"
+},
+{
+  question: chalk.cyanBright("Is she a writer? "),
+  answer:   "yes"
+},
+{
+  question: chalk.magentaBright("How many number of her poetries are on youtube?  "),
+  answer:   "5"
+}
+]
+for(var i=0;i<questionList.length;i++)
+{
+  console.log("----------")
+  var Question=questionList[i].question;
+  var Answer=questionList[i].answer;
+  play(Question,Answer);
+}
+
+console.log("------------------------------------------------")
+var ahead=readlinesync.question(chalk.red("Do you want to know your score? "));
+if(ahead.toUpperCase()=='YES')
+{
+  if(score>4)
+  console.log("Excellent!! Your score is 5 out of 5");
+  else if(score>=3)
+  console.log("Good!! Your score is "+ score+" out of 5");
+  else
+  console.log("Your score is "+ score+" out of 5")
+}
+}
